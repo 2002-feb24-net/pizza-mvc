fb224a16444505789a90f0da3d5dc0ac0aca7554
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-04-07 01:21:20 -0500

    added client side validation to stores

--- Restaurant.Library/Model/Store.cs
+++ Restaurant.Library/Model/Store.cs
@@ -8,14 +8,27 @@ namespace Restaurant.Domain.Model
 {
     public class Store : IDataStore
     {
-        [Display(Name = "Street Address")]
-        public string StreetAddress { get; set; }
         public int StoreId { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zipcode { get; set; }
+        [Required]
         [Display(Name = "Name")]
+        [DataType(DataType.Text)]
+
         public string StoreName { get; set; }
+        [Required]
+        [Display(Name = "Street Address")]
+        [DataType(DataType.MultilineText)]
+        public string StreetAddress { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+
+        public string City { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+
+        public string State { get; set; }
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public string Zipcode { get; set; }
 
 
         public Store()
