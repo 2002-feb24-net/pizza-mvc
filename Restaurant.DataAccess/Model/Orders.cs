3cbcfc97c6e1c2145912b7693eb1253cc156cef0
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-04-07 09:14:07 -0500

    fixed orders page error

--- Restaurant.DataAccess/Model/Orders.cs
+++ Restaurant.DataAccess/Model/Orders.cs
@@ -13,20 +13,20 @@ namespace Restaurant.DataAccess.Model
 
         public int OrderId { get; set; }
 
-        [Required]
-        [DataType(DataType.Currency)]
+        /*[Required]*/
+        /*[DataType(DataType.Currency)]*/
         public decimal? Total { get; set; }
 
         [Required]
-        [DataType(DataType.DateTime)]
+        /*[DataType(DataType.DateTime)]*/
         [Display(Name = "Time Ordered")]
         public DateTime TimeOrdered { get; set; }
 
-        [Required]
-        [Display(Name = "Customer ID")]
+        /*[Required]
+        [Display(Name = "Customer ID")]*/
         public int CustomerId { get; set; }
-        [Required]
-        [Display(Name = "Store ID")]
+        /*[Required]
+        [Display(Name = "Store ID")]*/
         public int StoreId { get; set; }
 
         public virtual Customers Customer { get; set; }
